7b365c18d06a2b3573ea26a986d38db798f253c0
Xoris650 <gabrielousadia12@gmail.com>
2023-10-09 19:39:01 -0300

    Adding 2 project

---
+++ src/ControleDeFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package ControleDeFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+    }
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
